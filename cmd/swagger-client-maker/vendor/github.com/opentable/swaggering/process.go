bc1a8204406ea68070f923d8e18595a028b90384
Judson <nyarly@gmail.com>
2016-07-15 15:41:17 -0700

    Unvendoring parent project

--- cmd/swagger-client-maker/vendor/github.com/opentable/swaggering/process.go
+++
@@ -1,9 +0,0 @@
-package swaggering
-
-func Process(packageName, serviceSource, renderTarget string) {
-	context := NewContext(packageName)
-
-	ProcessService(serviceSource, context)
-	ResolveService(context)
-	RenderService(renderTarget, context)
-}
