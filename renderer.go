777fa1bde2b6a2a20122426df17a73cc6771febb
Judson <nyarly@gmail.com>
2017-06-27 16:32:20 -0700

    Starting some cleanup of templates

--- renderer.go
+++ renderer.go
@@ -9,7 +9,8 @@ import (
 	"regexp"
 	"text/template"
 
-	"golang.org/x/tools/godoc/vfs"
+	"github.com/nyarly/inlinefiles/templatestore"
+
 	"golang.org/x/tools/imports"
 )
 
@@ -17,23 +18,20 @@ import (
 
 // Renderer is responsible for actually turning the parsed API into go code
 type Renderer struct {
-	targetDir                   string
-	modelTmpl, apiTmpl, dtoTmpl *template.Template
+	targetDir          string
+	modelTmpl, apiTmpl *template.Template
 }
 
 // NewRenderer creates a new renderer
-func NewRenderer(tgt string) (renderer *Renderer) {
-	renderer = &Renderer{targetDir: tgt}
+func NewRenderer(tgt string) *Renderer {
+	renderer := &Renderer{targetDir: tgt}
+	renderer.apiTmpl = template.Must(templatestore.LoadText(Templates, "api", "api.tmpl"))
+	opT := template.Must(templatestore.LoadTextOnto(Templates, renderer.apiTmpl, "operation", "operation.tmpl"))
+	template.Must(templatestore.LoadTextOnto(Templates, opT, "type", "type.tmpl"))
 
-	renderer.apiTmpl = template.Must(loadTemplate(Templates, "api", "api.tmpl"))
-	opT := renderer.apiTmpl.New("operation")
-	template.Must(loadTemplateInto(Templates, opT, "operation.tmpl"))
-	tyT := opT.New("type")
-	template.Must(loadTemplateInto(Templates, tyT, "type.tmpl"))
+	renderer.modelTmpl = template.Must(templatestore.LoadText(Templates, "model", "model.tmpl"))
 
-	renderer.modelTmpl = template.Must(loadTemplate(Templates, "model", "model.tmpl"))
-
-	return
+	return renderer
 }
 
 // RenderService performs the rendering of a service
@@ -55,40 +53,6 @@ func RenderService(target string, ingester *Context) {
 			log.Print(err)
 		}
 	}
-}
-
-func templateSource(fs vfs.Opener, fName string) (string, error) {
-	tmplFile, err := fs.Open(fName)
-	if err != nil {
-		return "", err
-	}
-	tmplB := &bytes.Buffer{}
-	_, err = tmplB.ReadFrom(tmplFile)
-	return tmplB.String(), err
-}
-
-func loadTemplate(fs vfs.Opener, tName, fName string) (*template.Template, error) {
-	return loadTemplateInto(fs, template.New(tName), fName)
-}
-
-func loadTemplateInto(fs vfs.Opener, tpl *template.Template, fName string) (*template.Template, error) {
-	src, err := templateSource(fs, fName)
-	if err != nil {
-		return nil, err
-	}
-	return tpl.Parse(src)
-}
-
-func apiPath(urlPath string) string {
-	slashes := regexp.MustCompile("/+")
-	unders := regexp.MustCompile("^_+")
-	brackets := regexp.MustCompile("[}{]")
-
-	urlPath = slashes.ReplaceAllString(urlPath, "_")
-	urlPath = unders.ReplaceAllString(urlPath, "")
-	urlPath = brackets.ReplaceAllString(urlPath, "")
-
-	return urlPath
 }
 
 func snakeCase(symbol string) string {
